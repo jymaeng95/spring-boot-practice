176af16af7f0cfa63bd5847f02155e817aa1a886
zayson <jymaeng95@gmail.com>
2022-07-13 17:55:21 +0900

    feat: Stream API 사용 연습

---
+++ practice/src/main/java/com/spring/practice/steram/SortedStream.java
@@ -0,0 +1,34 @@
+package com.spring.practice.steram;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class SortedStream {
+    @Data
+    @AllArgsConstructor
+    static class Person {
+        private String name;
+        private int age;
+    }
+
+    public static void main(String[] args) {
+        List<Person> personList = List.of(
+                new Person("zayson", 28),
+                new Person("chaeyoung", 26),
+                new Person("maeng", 30));
+
+        // sorted : 스트림 데이터를 정렬해 스트림으로 반환 (파라미터 : comparator, Comparable)
+        // 이름을 기준으로 오름차순 정렬
+        personList.stream()
+                .sorted(Comparator.comparing(Person::getName))
+                .forEach(System.out::println);
+
+        // 나이를 기준으로 내림차순 정렬
+        personList.stream()
+                .sorted(Comparator.comparingInt(Person::getAge).reversed())
+                .forEach(System.out::println);
+    }
+}
