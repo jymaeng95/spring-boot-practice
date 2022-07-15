8428ef8a831ff7aa86e6f1506f1aae70b3c9999c
zayson <jymaeng95@gmail.com>
2022-07-15 13:44:39 +0900

    feat: Kotlin 변수, null 처리 사용 예제

---
+++ practice/src/main/java/zayson/java/lab/kotlin/nullable/Person.java
@@ -0,0 +1,15 @@
+package zayson.java.lab.kotlin.nullable;
+
+import org.jetbrains.annotations.Nullable;
+
+public class Person {
+    private final String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ practice/src/main/java/zayson/java/lab/kotlin/variable/Person_Java.java
@@ -0,0 +1,9 @@
+package zayson.java.lab.kotlin.variable;
+
+public class Person_Java {
+    private String name;
+
+    public Person_Java(String name) {
+        this.name = name;
+    }
+}
