d527bcd1b7f0595a02764e671ac1184436b16241
zayson <jymaeng95@gmail.com>
2022-08-24 10:19:00 +0900

    feat: Kotlin In Action [2장 코틀린 기초] 스마트 캐스트

--- practice/src/main/java/zayson/java/lab/kotlin/basic/스마트_캐스트.kt
+++ practice/src/main/java/zayson/java/lab/kotlin/basic/스마트_캐스트.kt
@@ -3,20 +3,35 @@ package zayson.java.lab.kotlin.basic
 import java.lang.IllegalArgumentException
 
 fun main(args: Array<String>) {
+    val rst = eval(Sum(Num(1), Num(2)))
+    println("rst = ${rst}")
+
+    val rst2 = evalIf(Sum(Num(1), Num(2)))
+    println("rst2 = ${rst2}")
+
 
 }
 
 fun eval(e: Expr): Int {
+    // e가 Num 인스턴스인 경우
     if(e is Num) {
-        val n = e as Num
-        return e.value
-    }
-    if(e is Sum) {
-        val left = e.left as Num
-        val right = e.right as Num
 
-        return left.value + right.value
+        val n = e as Num  // 스마트 캐스트로 생략 가능
+        return n.value
     }
+
+    // e가 Sum 인스턴스인 경우
+    if(e is Sum)
+        return eval(e.left) + eval(e.right)
+
 
     throw IllegalArgumentException("예외 발생")
 }
+
+// 식을 본문으로 사용하기
+fun evalIf(e: Expr): Int =
+    if (e is Num) e.value
+    else if (e is Sum) evalIf(e.left) + evalIf(e.right)
+    else throw IllegalArgumentException("예외 발생")
+
+// when 절로 사용하기
