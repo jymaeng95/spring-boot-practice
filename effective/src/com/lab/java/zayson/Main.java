2bb4c9994965de955f64ec9c3ece5d9fa041622a
zayson <jymaeng95@gmail.com>
2022-11-09 15:57:57 +0900

    init: effective initial Commit

---
+++ effective/src/com/lab/java/zayson/Main.java
@@ -0,0 +1,11 @@
+package com.lab.java.zayson;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here \
+        System.out.println("effective test");
+        System.out.println("effective test");
+
+    }
+}
