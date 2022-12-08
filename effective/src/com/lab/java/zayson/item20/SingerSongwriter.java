70984115f6f063124335a1cf55cbdacc51e044c5
zayson <jymaeng95@gmail.com>
2022-12-08 22:52:00 +0900

    feat: [이펙티브 자바] Item 20 - 인터페이스는 계층 구조가 없는 예제

--- effective/src/com/lab/java/zayson/item20/SingerSongwriter.java
+++ effective/src/com/lab/java/zayson/item20/SingerSongwriter.java
@@ -3,7 +3,7 @@ package com.lab.java.zayson.item20;
 public interface SingerSongwriter extends Singer, Songwriter{
     // 현실세계에는 Singer, Songwriter를 모두 다 하는 SingerSongwirter가 존재
     // 하지만 Singer, Songwriter가 계층 구조가 존재한다고 보기어렵다.
-
+    
     void strum();
 
     void actSensitive();
