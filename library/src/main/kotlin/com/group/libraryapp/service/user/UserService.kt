233768e5be0a685d62e95a9a296bfbe2d49733cd
zayson <jymaeng95@gmail.com>
2023-05-05 18:18:28 +0900

    refactor: Java Service -> Kotlin Service 리팩토링

---
+++ library/src/main/kotlin/com/group/libraryapp/service/user/UserService.kt
@@ -0,0 +1,41 @@
+package com.group.libraryapp.service.user
+
+import com.group.libraryapp.domain.user.User
+import com.group.libraryapp.domain.user.UserRepository
+import com.group.libraryapp.dto.user.request.UserCreateRequest
+import com.group.libraryapp.dto.user.request.UserUpdateRequest
+import com.group.libraryapp.dto.user.response.UserResponse
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class UserService(
+    private val userRepository: UserRepository,
+) {
+    /**
+     * @Transactional을 사용하려면 메서드가 오버라이딩 가능해야함 (Transactional Proxy 기반 AOP를 통해 동작 -> Proxy를 위해 상속 및 오버라이딩을 통한 조작)
+     * 상속 및 오버라이딩을 해주기 위해 open을 붙여줄 필요가 있음
+     * open을 매번 붙여주기 어려우므로 플러그인으로 대체
+     */
+    @Transactional
+    open fun saveUser(request: UserCreateRequest) {
+        val newUser = User(request.name, request.age)
+        userRepository.save(newUser)
+    }
+
+    @Transactional(readOnly = true)
+    fun getUsers(): List<UserResponse> = userRepository.findAll()
+        .map(::UserResponse)
+
+    @Transactional
+    fun updateUserName(request: UserUpdateRequest) {
+        val user = userRepository.findById(request.id).orElseThrow(::IllegalArgumentException)
+        user.updateName(request.name)
+    }
+
+    @Transactional
+    fun deleteUser(name: String) {
+        val user = userRepository.findByName(name).orElseThrow(::IllegalArgumentException)
+        userRepository.delete(user)
+    }
+}
