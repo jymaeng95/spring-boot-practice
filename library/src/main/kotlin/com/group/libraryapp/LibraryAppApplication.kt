ac1cb5e04b0d5683e4af17293caec1d3290a5f04
zayson <jymaeng95@gmail.com>
2023-05-06 15:33:14 +0900

    refactor: Java SpringBoot Application -> Kotlin SpringBoot Application 리팩토링

---
+++ library/src/main/kotlin/com/group/libraryapp/LibraryAppApplication.kt
@@ -0,0 +1,9 @@
+package com.group.libraryapp
+
+import org.springframework.boot.runApplication
+
+class LibraryAppApplication
+
+fun main(args: Array<String>) {
+    runApplication<LibraryAppApplication>(*args)
+}
